bd6f2effac4d6a8f9344bb1ba35a0e21de4dc583
Paul D'Angio <pcdangio@gmail.com>
2019-07-23 22:01:36 -0400

    Partial implementation of message.

--- src/message.cpp
+++ src/message.cpp
@@ -1,6 +1,141 @@
 #include "message.h"
 
-message::message()
+#include <sstream>
+#include <iomanip>
+
+// CONSTRUCTORS
+message::message(id command_id)
+{
+    // Create empty message with specified ID.
+    message::m_talker = message::talker::PMTK;
+    message::m_id = command_id;
+}
+message::message(std::string serialized)
+{
+    // Deserialize message string.
+
+    // Determine talker.
+    // Check beginning of packet to determine talker.
+    // Most messages will be $GP
+    std::string sub_talker = serialized.substr(0, 3);
+    if(sub_talker.compare("$GP") == 0)
+    {
+        message::m_talker = message::talker::NMEA;
+    }
+
+    std::string sub_talker = serialized.substr(1, 4);
+    if(sub_talker.compare("PMTK") == 0)
+    {
+        message::m_talker = message::talker::PMTK;
+    }
+    else if(sub_talker.compare("NMEA") == 0)
+    {
+        message::m_talker = message::talker::NMEA;
+    }
+    else
+    {
+        message::m_talker = message::talker::UNKNOWN;
+    }
+
+    // Read command ID.
+    try
+    {
+        message::m_id = static_cast<message::id>(std::stoi(serialized.substr(5, 3)));
+    }
+    catch(...)
+    {
+        message::m_id = message::id::UNKNOWN;
+    }
+
+    // Read data fields.
+    unsigned long data_end = serialized.find('*');
+    unsigned long data_length = data_end - 8;
+    $PMTK000,1,2,3*
+    01234567891
+
+    if(data_end != 8)
+    {
+        // Data fields present.
+        // Find location of data field end.
+        std::string sub_data = serialized.substr(9, )
+    }
+    unsigned long position = serialized.find(',');
+    while(position != std::string::npos)
+    {
+        unsigned long start = position;
+        // Find the next comma position.
+        position = serialized.find(',');
+        // Extract current data field.
+        message::m_data.push_back(serialized.substr(position, ))
+    }
+}
+
+// METHODS
+void message::add_field(unsigned int data)
 {
 
+}
+unsigned int message::get_field(unsigned int address) const
+{
+
+}
+std::string message::calculate_checksum() const
+{
+
+}
+bool message::validate_checksum() const
+{
+
+}
+std::string message::serialize() const
+{
+    // Serialize message into string.
+    std::stringstream stream;
+    // Add header.
+    stream << '$';
+    // Add talker type.
+    switch(message::m_talker)
+    {
+    case message::talker::UNKNOWN:
+    {
+        stream << "UNKN";
+        break;
+    }
+    case message::talker::NMEA:
+    {
+        stream << "NMEA";
+        break;
+    }
+    case message::talker::PMTK:
+    {
+        stream << "PMTK";
+        break;
+    }
+    }
+    // Add message ID, with field width of 3.
+    stream << std::setw(3) << std::setfill('0') << static_cast<unsigned int>(message::m_id);
+    stream.copyfmt(std::ios(nullptr));
+    // Add data fields.
+    for(unsigned int n = 0; n < message::m_data.size(); n++)
+    {
+        stream << ',' << message::m_data.at(n);
+    }
+    // Add data end marker
+    stream << '*';
+    // Add checksum.
+    stream << message::calculate_checksum();
+    // Add footer.
+    stream << "\r\n";
+
+    return stream.str();
+}
+
+// PROPERTIES
+message::talker message::p_talker() const
+{
+    return message::m_talker;
+}
+message::id message::p_id() const
+{
+    return message::m_id;
 }
--- src/message.h
+++ src/message.h
@@ -7,26 +7,46 @@
 class message
 {
 public:
+    enum class talker
+    {
+        UNKNOWN = 0,
+        PMTK = 1,
+        NMEA = 2
+    };
     enum class id
     {
+        UNKNOWN =                   000,
         PMTK_ACK =                  001,
         PMTK_SYS_MSG =              010,
+        PMTK_TXT_MSG =              011,
         PMTK_SET_NMEA_UPDATERATE =  220,
         PMTK_SET_NMEA_BAUDRATE =    251,
         PMTK_SET_API_NMEA_OUTPUT =  314
     };
+
+
     message(id command_id);
     message(std::string serialized);
 
-    void add_field(int data);
+    void add_field(unsigned int data);
+    unsigned int get_field(unsigned int address) const;
 
-    std::string serialize();
+    std::string serialize() const;
+
+
+    talker p_talker() const;
+    id p_id() const;
 
 private:
+    talker m_talker;
     id m_id;
     std::vector<std::string> m_data;
 
+    void parse_pmtk(std::string serialized);
+    void parse_nmea(std::string serialized);
 
+    std::string calculate_checksum() const;
+    bool validate_checksum() const;
 };
 
 #endif // MESSAGE_H
